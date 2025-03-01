ffbcf52f688513e3597351c147674d9edafe1a5e
Treelar <me@treelar.xyz>
2025-02-28 18:03:48 -0800

    migrations + dtos

--- Xiaolongshu2Model/WorldcitiesSrcContext.cs
+++ Xiaolongshu2Model/WorldcitiesSrcContext.cs
@@ -21,14 +21,12 @@ public partial class WorldcitiesSrcContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb;database=worldcities_src");
+        => optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb;database=worldcities");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<City>(entity =>
         {
-            entity.Property(e => e.Id).ValueGeneratedNever();
-
             entity.HasOne(d => d.Country).WithMany(p => p.Cities)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_City_Country");
